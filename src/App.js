08f5bf72247c4dc69915d947c62dbf1b53f4283a
chelseawangsf <chelseawangsf@gmail.com>
2018-02-10 05:50:02 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -2,63 +2,44 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import Rx from 'rxjs/Rx';
 import './App.css';
-import { Event, Stream, Pipeline, TimeEvent, TimeSeries, TimeRange } from 'pondjs';
-import Ring from 'ringjs';
-import { ChartContainer, ChartRow, YAxis, Charts, LineChart } from 'react-timeseries-charts';
-import moment from 'moment';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      initialTime: new Date(),
-      latestTime: new Date(),
-      events: new Ring(200),
-    }
-  }
+import PropTypes from 'prop-types';
 
-  componentWillMount() {
-    this.stream = new Stream();
-    const values$ = Rx.Observable.interval(60).map(x => ({value: Math.random() * 12000}));
-    values$.subscribe(value => {
-      const newEvents = this.state.events;
-      const time = new Date();
-      const event = new TimeEvent(time, value);
-      newEvents.push(event);
-      this.setState({
-        latestTime: time,
-        events: newEvents
-      });
-      this.stream.addEvent(event);
-    })
-  }
+import Typography from 'material-ui/styles';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import TimeSeriesChart from './components/TimeSeriesChart';
 
-  render() {
-    const rawSeries = new TimeSeries({
-      name: 'raw',
-      events: this.state.events.toArray()
-    });
-    let timeRange = rawSeries.timerange();
-    if (!timeRange) {
-      timeRange = new TimeRange(this.state.initialTime, this.state.latestTime);
-    }
-    return (
-      <ChartContainer timeRange={timeRange}>
-        <ChartRow height='150'>
-          <YAxis
-            id='y'
-            label='Value'
-            min={0} max={15000}
-            type='linear' />
-          <Charts>
-            <LineChart
-              axis='y'
-              series={rawSeries} />
-          </Charts>
-        </ChartRow>
-      </ChartContainer>
-    );
-  }
-}
+import Header from './components/Header';
+import Toolbar from 'material-ui/Toolbar';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from 'material-ui-icons/Menu';
+import * as Colors from 'material-ui/styles/colors'
+import { grey800 } from 'material-ui/styles/colors';
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: grey800,
+    
+  },
+  appBar: {
+    height: 40,
+  },
+});
+
+
+const App = () => (
+  <MuiThemeProvider muiTheme={muiTheme}>
+    <div clasName="container">
+      <div className="row">
+      <Header/>
+      </div>
+      <TimeSeriesChart />
+    </div>
+
+
+  </MuiThemeProvider>
+);
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {AppBar, Tabs, Tab} from 'material-ui'
+
+class Header extends React.Component {
+  render() {
+    return (
+      <AppBar title="Test Page">
+        
+      </AppBar>
+    )
+  }
+}
+export default Header;
